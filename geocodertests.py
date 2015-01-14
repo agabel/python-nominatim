134792fb435f20a552b438860da2d86347ec8d0d
Clay Keller <ckeller@nex-tech.com>
2015-01-14 14:32:25 -0600

    Made the tests a little more specific when validating the response.

--- geocodertests.py
+++ geocodertests.py
@@ -10,12 +10,15 @@ class GeocoderTestCase(unittest.TestCase):
     def test_geocode_city(self):
         client = nominatim.Geocoder()
         response = client.geocode(city_list.us)
-        self.assertIsNotNone(response)
+        self.assertEquals(response[0]['lon'], '-99.3267702')
+        self.assertEquals(response[0]['lat'], '38.8791783')
 
     def test_get_location_by_city_invalid_chars(self):
         client = nominatim.Geocoder()
+        # we need to test that the value is unicode and properly converted to a string when it is processed
         response = client.geocode(unicode(city_list.intl, 'utf-8'))
-        self.assertIsNotNone(response)
+        self.assertEquals(response[0]['lon'], '-3.9868641')
+        self.assertEquals(response[0]['lat'], '48.6849601')
 
 
 if __name__ == "__main__":
